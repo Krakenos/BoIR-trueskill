b895b997873ffd06aaf3284851333b0a2792dfec
Krakenos <d.123@wp.pl>
2017-11-10 17:51:09 +0100

    Add files via upload

---
+++ json_creator.py
@@ -0,0 +1,48 @@
+import json
+import requests
+
+
+def json_parser(matches, participants, tourney_id):
+    parsed_json = {"name": tourney_id,
+                   "challonge": "http://challonge.com/" + tourney_id,
+                   "ruleset": "",
+                   "matchups": []
+                   }
+    for match in matches:
+        match_data = match['match']
+        parsed_json["matchups"].append({"player 1": match_data['player1_id'],
+                                        "player 2": match_data['player2_id'],
+                                        "winner": match_data['winner_id']})
+    for participant in participants:
+        participant_id = participant['participant']['id']
+        participant_name = participant['participant']['name']
+
+        for match in parsed_json["matchups"]:
+            if match['player 1'] == participant_id:
+                match['player 1'] = participant_name
+                if match['winner'] == participant_id:
+                    match['winner'] = '1'
+            if match['player 2'] == participant_id:
+                match['player 2'] = participant_name
+                if match['winner'] == participant_id:
+                    match['winner'] = '2'
+    print("json parsed successfully")
+    return parsed_json
+
+
+n = 0
+while True:
+    n = n + 1
+    k = str(n)
+    api_key = ''
+    print("Type q to exit")
+    tourney_id = input("enter the tournament id:")
+    if tourney_id == 'q':
+        break
+    participants = requests.get("https://api.challonge.com/v1/tournaments/" + tourney_id + "/participants.json",
+                                params={'api_key': api_key}).json()
+    matches = requests.get("https://api.challonge.com/v1/tournaments/" + tourney_id + "/matches.json",
+                           params={'api_key': api_key}).json()
+    json_var = json_parser(matches, participants, tourney_id)
+    with open('tournaments/' + k + '_unasigned.json', 'w') as data_file:
+        json.dump(json_var, data_file, indent=2)
---
+++ main.py
@@ -0,0 +1,50 @@
+import json
+from trueskill import Rating, rate_1vs1
+
+
+
+def calculate_places(racers):
+    leaderboards_list = [{'name': key, 'exposure': value.exposure, 'mu': value.mu} for key, value in racers.items()]
+    leaderboards_list.sort(key=lambda x: x['exposure'], reverse=True)
+    for place, player in enumerate(leaderboards_list):
+        player['place'] = place + 1
+    return leaderboards_list
+
+
+def calculate_mmr(matchup, racers_list):
+    player_1 = matchup['player 1'].lower()
+    player_2 = matchup['player 2'].lower()
+    winner = matchup['winner']
+    if player_1 not in racers_list:
+        racers_list[player_1] = Rating(25)
+    if player_2 not in racers_list:
+        racers_list[player_2] = Rating(25)
+    if winner == '1':
+        racers_list[player_1], racers_list[player_2] = rate_1vs1(racers_list[player_1], racers_list[player_2])
+    if winner == '2':
+        racers_list[player_2], racers_list[player_1] = rate_1vs1(racers_list[player_2], racers_list[player_1])
+    return racers_list
+
+
+try:
+    racers = {}
+    n = 0
+    while True:
+        n = n + 1
+        with open('tournaments/' + str(n)+'.json') as datafile:
+            tournament_data = json.load(datafile)
+        race_list = tournament_data['matchups']
+        for race in race_list:
+            if tournament_data['ruleset'] == 'seeded':
+                for i in range(3):
+                    calculate_mmr(race, racers)
+            else:
+                calculate_mmr(race, racers)
+
+except FileNotFoundError:
+    leaderboard = calculate_places(racers)
+    with open('leaderboard.json', 'w') as output:
+        json.dump(leaderboard, output, indent=2)
+    print("Place    Name    TrueSkill")
+    for record in leaderboard:
+        print(f"{record['place']}   {record['name']}    {round(record['exposure'], 2)}")
