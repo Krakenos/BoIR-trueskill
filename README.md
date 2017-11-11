1e24b02d361e11718a5079b52500b533529868a7
Krakenos <bylebezdurnychcyferek@gmail.com>
2017-11-11 15:14:33 +0100

    Fixing some typos, adding ruleset to few JSON files

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # BoIR-trueskill
 
-Program written in Python 3.6.6 used to import brackets from challonge.com and to calculate trueskill rating based on individual match ups.
+Program written in Python 3.6.1 used to import brackets from [Challonge](http://challonge.com/) and to calculate [TrueSkill](http://trueskill.org/) rating based on individual match ups.
 
-To import bracket from challonge paste your challonge api key into api_key variable in json_creator.py
+To import bracket from [Challonge](http://challonge.com/) paste your Challonge API key into 'api_key' variable in 'json_creator.py'.
 
-main.py goes through .json files in "tournaments" directory in order.
+'main.py' goes through JSON files in 'tournaments' directory in order.
 
-Program is not even close to finished
+Program is not even close to finished.
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import json
 from trueskill import Rating, rate_1vs1
 
 
-
-def calculate_places(racers):
-    leaderboards_list = [{'name': key, 'exposure': value.exposure, 'mu': value.mu} for key, value in racers.items()]
+def calculate_places(racers_dict):
+    leaderboards_list = [{'name': key, 'exposure': value.exposure, 'mu': value.mu} for key, value in
+                         racers_dict.items()]
     leaderboards_list.sort(key=lambda x: x['exposure'], reverse=True)
     for place, player in enumerate(leaderboards_list):
         player['place'] = place + 1
@@ -21,17 +21,17 @@ def calculate_mmr(matchup, racers_list):
         racers_list[player_2] = Rating(25)
     if winner == '1':
         racers_list[player_1], racers_list[player_2] = rate_1vs1(racers_list[player_1], racers_list[player_2])
-    if winner == '2':
+    elif winner == '2':
         racers_list[player_2], racers_list[player_1] = rate_1vs1(racers_list[player_2], racers_list[player_1])
     return racers_list
 
 
+racers = {}
 try:
-    racers = {}
     n = 0
     while True:
         n = n + 1
-        with open('tournaments/' + str(n)+'.json') as datafile:
+        with open('tournaments/' + str(n) + '.json') as datafile:
             tournament_data = json.load(datafile)
         race_list = tournament_data['matchups']
         for race in race_list:
