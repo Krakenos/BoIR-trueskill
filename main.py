8abd4444aa55afc89ad75ab7dfe6e440a4cba874
Krakenos <bylebezdurnychcyferek@gmail.com>
2017-11-15 16:38:10 +0100

    Added debug flag, changed seeded to 4 match ups insteaad of 3

--- main.py
+++ main.py
@@ -19,27 +19,40 @@ def calculate_mmr(matchup, racers_list):
         racers_list[player_1] = Rating(25)
     if player_2 not in racers_list:
         racers_list[player_2] = Rating(25)
+    old_exposure_1 = racers_list[player_1].exposure
+    old_exposure_2 = racers_list[player_2].exposure
     if winner == '1':
         racers_list[player_1], racers_list[player_2] = rate_1vs1(racers_list[player_1], racers_list[player_2])
+        if debug:
+            print(player_1 + ' ' + player_2 + ' ' + str(racers_list[player_1].exposure) + ' ' + str(
+                racers_list[player_2].exposure) + ' +' + str(
+                racers_list[player_1].exposure - old_exposure_1) + ' ' + str(
+                racers_list[player_2].exposure - old_exposure_2))
     elif winner == '2':
         racers_list[player_2], racers_list[player_1] = rate_1vs1(racers_list[player_2], racers_list[player_1])
+        if debug:
+            print(player_2 + ' ' + player_1 + ' ' + str(racers_list[player_2].exposure) + ' ' + str(
+                racers_list[player_1].exposure) + ' +' + str(
+                racers_list[player_2].exposure - old_exposure_2) + ' ' + str(
+                racers_list[player_1].exposure - old_exposure_1))
     return racers_list
 
 
 def print_leaderboard(leaderboard_json):
-    max_name_lenght = 0
+    max_name_length = 0
     for record in leaderboard_json:
-        if len(str(record['name'])) > max_name_lenght:
-            max_name_lenght = len(str(record['name']))
-    print('Place  ' + 'Name' + (max_name_lenght-2)*' ' + 'Trueskill')
-    print('-'*6 + '-'*max_name_lenght + '-'*12)
+        if len(str(record['name'])) > max_name_length:
+            max_name_length = len(str(record['name']))
+    print('Place  ' + 'Name' + (max_name_length - 2) * ' ' + 'Trueskill')
+    print('-' * 6 + '-' * max_name_length + '-' * 12)
     for record in leaderboard_json:
         place = str(record['place'])
         name = record['name']
         exposure = str(round(record['exposure'], 2))
-        print('#' + place + (5-len(place))*' ' + ' ' + name + (max_name_lenght-len(name)+2)*' ' + exposure)
+        print('#' + place + (5 - len(place)) * ' ' + ' ' + name + (max_name_length - len(name) + 2) * ' ' + exposure)
 
 
+debug = False  # Changing to True will cause printing out exposure change for each match up
 racers = {}
 try:
     n = 0
@@ -50,12 +63,18 @@ try:
         race_list = tournament_data['matchups']
         for race in race_list:
             if tournament_data['ruleset'] == 'seeded':
-                for i in range(3):
+                if debug:
+                    print('seeded')
+                for i in range(4):
                     calculate_mmr(race, racers)
             elif tournament_data['ruleset'] == 'mixed':
+                if debug:
+                    print('mixed')
                 for i in range(2):
                     calculate_mmr(race, racers)
             else:
+                if debug:
+                    print('unseeded')
                 calculate_mmr(race, racers)
 
 except FileNotFoundError:
