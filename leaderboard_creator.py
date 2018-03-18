3aee147d522181d874d82d6d4c115956a24d9970
Krakenos <1944193+Krakenos@users.noreply.github.com>
2018-03-18 22:33:46 +0100

    lots of changes
    added data for tournament and match participation to the leaderboards, now people with 1 tournament participation are hidden from leaderboards, small clean up in the code

--- leaderboard_creator.py
+++ leaderboard_creator.py
@@ -1,20 +1,24 @@
 import json
 import glob
+from collections import defaultdict
 from trueskill import Rating, rate_1vs1
 
 debug = False  # Changing to True will cause printing out exposure change for each match up
 current_file = ''  # Global variable to track which file is being processed, used in name_check function
 calculate_per_round = False
+tournament_limit = 2  # Hides people who played low amount of tourneys from the leaderboards
 seeded_multiplier = 4
 
 
 def main():
-    racers = {}
-    seeded_racers = {}
-    unseeded_racers = {}
+    racers = defaultdict(dict)
+    seeded_racers = defaultdict(dict)
+    unseeded_racers = defaultdict(dict)
 
     # Looping through every file in the tournaments folder
     for infile in sorted(glob.glob('tournaments/*.json')):
+        players_in_tourney_1 = []
+        players_in_tourney_2 = []  # 2 variables because 1 tourney counts for 2 leaderboards generally
         global current_file
         current_file = infile
         with open(infile) as datafile:
@@ -24,6 +28,12 @@ def main():
         # Looping through every race in the tournament
         for race in race_list:
             if tournament_data['ruleset'] == 'seeded':
+                check_racer(race, racers)
+                check_racer(race, seeded_racers)
+                increment_tourney_played(race, racers, players_in_tourney_1)
+                increment_tourney_played(race, seeded_racers, players_in_tourney_2)
+                increment_match_played(race, racers)
+                increment_match_played(race, seeded_racers)
                 if debug:
                     print('seeded')
                 for i in range(seeded_multiplier):
@@ -36,6 +46,12 @@ def main():
                 else:
                     calculate_mmr(race, seeded_racers)
             elif tournament_data['ruleset'] == 'mixed':
+                check_racer(race, racers)
+                check_racer(race, unseeded_racers)
+                increment_tourney_played(race, racers, players_in_tourney_1)
+                increment_tourney_played(race, unseeded_racers, players_in_tourney_2)
+                increment_match_played(race, racers)
+                increment_match_played(race, unseeded_racers)
                 if debug:
                     print('mixed')
                 if calculate_per_round is True:
@@ -48,6 +64,12 @@ def main():
             elif tournament_data['ruleset'] == 'other':
                 continue
             else:
+                check_racer(race, racers)
+                check_racer(race, unseeded_racers)
+                increment_tourney_played(race, racers, players_in_tourney_1)
+                increment_tourney_played(race, unseeded_racers, players_in_tourney_2)
+                increment_match_played(race, racers)
+                increment_match_played(race, unseeded_racers)
                 if debug:
                     print('unseeded')
                 if calculate_per_round is True:
@@ -66,18 +88,42 @@ def main():
     print_leaderboard(mixed_leaderboard)
 
 
+def increment_tourney_played(matchup, racers_dict, tourney_players):
+    winner = name_check(matchup['winner'], racers_dict)
+    loser = name_check(matchup['loser'], racers_dict)
+    if winner not in tourney_players:
+        tourney_players.append(winner)
+        racers_dict[winner]['tournaments_played'] += 1
+    if loser not in tourney_players:
+        tourney_players.append(loser)
+        racers_dict[loser]['tournaments_played'] += 1
+
+
 def calculate_places(racers_dict):
     # Creating leaderboard, sorting by exposure value
-    leaderboards_list = [{'name': key, 'exposure': value.exposure, 'mu': value.mu, 'sigma': value.sigma}
-                         for key, value in racers_dict.items()]
+    leaderboards_list = [
+        {
+            'name': key,
+            'tournaments_played': value['tournaments_played'],
+            'matches_played': value['matches_played'],
+            'exposure': value['rating'].exposure,
+            'mu': value['rating'].mu,
+            'sigma': value['rating'].sigma
+        } for key, value in racers_dict.items() if value['tournaments_played'] >= tournament_limit
+    ]
     leaderboards_list.sort(key=lambda x: x['exposure'], reverse=True)
     for place, player in enumerate(leaderboards_list):
         player['place'] = place + 1
     return leaderboards_list
 
 
+def adjust_trueskill(winner, loser, racers_dict):
+    racers_dict[winner]['rating'], racers_dict[loser]['rating'] = \
+        rate_1vs1(racers_dict[winner]['rating'], racers_dict[loser]['rating'])
+
+
 def name_check(name, racers_dict):
-    for key, value in racers_dict.items():
+    for key, _ in racers_dict.items():
         if name.lower() == key.lower():
             if name != key:
                 print(f'WARNING: expected name {key} got {name} in {current_file} file')
@@ -85,30 +131,42 @@ def name_check(name, racers_dict):
     return name
 
 
-def calculate_mmr(matchup, racers_list):
-    winner = name_check(matchup['winner'], racers_list)
-    loser = name_check(matchup['loser'], racers_list)
-    if winner not in racers_list:  # If the person doesn't have existing rating, assign rating of 25
-        racers_list[winner] = Rating(25)
-    if loser not in racers_list:
-        racers_list[loser] = Rating(25)
+def check_racer(matchup, racers_dict):
+    winner = name_check(matchup['winner'], racers_dict)
+    loser = name_check(matchup['loser'], racers_dict)
+    if winner not in racers_dict:  # If the person doesn't have existing rating, assign rating of 25
+        racers_dict[winner]['rating'] = Rating(25)
+        racers_dict[winner]['matches_played'] = 0
+        racers_dict[winner]['tournaments_played'] = 0
+    if loser not in racers_dict:
+        racers_dict[loser]['rating'] = Rating(25)
+        racers_dict[loser]['matches_played'] = 0
+        racers_dict[loser]['tournaments_played'] = 0
 
-    old_exposure_1 = racers_list[winner].exposure  # Variables for debug purpose
-    old_exposure_2 = racers_list[loser].exposure
+
+def increment_match_played(matchup, racers_dict):
+    winner = name_check(matchup['winner'], racers_dict)
+    loser = name_check(matchup['loser'], racers_dict)
+    racers_dict[winner]['matches_played'] += 1
+    racers_dict[loser]['matches_played'] += 1
+
+
+def calculate_mmr(matchup, racers_dict):
+    winner = name_check(matchup['winner'], racers_dict)
+    loser = name_check(matchup['loser'], racers_dict)
+    old_exposure_1 = racers_dict[winner]['rating'].exposure  # Variables for debug purpose
+    old_exposure_2 = racers_dict[loser]['rating'].exposure
 
     if not (matchup['score'] == 'draw'):  # We want to ignore draws because they most likely are mistake in the json.
-
-        # Adjusting trueskill
-        racers_list[winner], racers_list[loser] = rate_1vs1(racers_list[winner], racers_list[loser])
-
+        adjust_trueskill(winner, loser, racers_dict)
     if debug:
         # Printing out every change in the exposure value
-        print(winner + ' ' + loser + ' ' + str(racers_list[winner].exposure) + ' ' + str(
-            racers_list[loser].exposure) + ' +' + str(
-            racers_list[winner].exposure - old_exposure_1) + ' ' + str(
-            racers_list[loser].exposure - old_exposure_2))
+        print(winner + ' ' + loser + ' ' + str(racers_dict[winner]['rating'].exposure) + ' ' + str(
+            racers_dict[loser]['rating'].exposure) + ' +' + str(
+            racers_dict[winner]['rating'].exposure - old_exposure_1) + ' ' + str(
+            racers_dict[loser]['rating'].exposure - old_exposure_2))
 
-    return racers_list
+    return racers_dict
 
 
 def print_leaderboard(leaderboard_json):
@@ -133,30 +191,26 @@ def dump_json(filename, data):
 def mmr_per_round(race, racers_dict, mixed_flag=False):
     winner = name_check(race['winner'], racers_dict)
     loser = name_check(race['loser'], racers_dict)
-    if winner not in racers_dict:
-        racers_dict[winner] = Rating(25)
-    if loser not in racers_dict:
-        racers_dict[loser] = Rating(25)
     if race['score'] != 'draw':
         if mixed_flag is False:
             scores = race['score'].split('-')
             for i in range(int(scores[1])):
-                racers_dict[loser], racers_dict[winner] = rate_1vs1(racers_dict[loser], racers_dict[winner])
+                adjust_trueskill(loser, winner, racers_dict)
             for i in range(int(scores[0])):
-                racers_dict[winner], racers_dict[loser] = rate_1vs1(racers_dict[winner], racers_dict[loser])
+                adjust_trueskill(winner, loser, racers_dict)
         else:
             for round in race['ruleset_per_round']:
                 if round['ruleset'] == 'seeded':
                     for i in range(seeded_multiplier):
                         if winner == round['winner']:
-                            racers_dict[winner], racers_dict[loser] = rate_1vs1(racers_dict[winner], racers_dict[loser])
+                            adjust_trueskill(winner, loser, racers_dict)
                         else:
-                            racers_dict[loser], racers_dict[winner] = rate_1vs1(racers_dict[loser], racers_dict[winner])
+                            adjust_trueskill(loser, winner, racers_dict)
                 else:
                     if winner == round['winner']:
-                        racers_dict[winner], racers_dict[loser] = rate_1vs1(racers_dict[winner], racers_dict[loser])
+                        adjust_trueskill(winner, loser, racers_dict)
                     else:
-                        racers_dict[loser], racers_dict[winner] = rate_1vs1(racers_dict[loser], racers_dict[winner])
+                        adjust_trueskill(loser, winner, racers_dict)
 
 
 if __name__ == '__main__':
